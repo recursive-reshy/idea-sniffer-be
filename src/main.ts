961d428766c548609a48ef7be3e77ec81a5a93a8
recursive-reshy <61969021+recursive-reshy@users.noreply.github.com>
2026-05-10 13:26:05 +0800

    feat: Added Fastify http package

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core'
+// Http Addpater'
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify'
+// App
 import { AppModule } from './app.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create( AppModule )
+  const app = await NestFactory.create< NestFastifyApplication >( 
+    AppModule, 
+    new FastifyAdapter() 
+  )
+
+  app.setGlobalPrefix( 'api/v1' )
+
   await app.listen( process.env.PORT ?? 3000 )
 }
 
