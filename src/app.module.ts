dac5bd17c7cbda4cc78c402b6eccb4ada6840897
recursive-reshy <61969021+recursive-reshy@users.noreply.github.com>
2026-05-10 20:50:05 +0800

    feat: Added Config module to load env variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,18 @@
 // Nest
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
 // Modules
 import { CommonHttpModule } from './common/http/http.module'
+import { RunsModule } from './runs/runs.module'
 // Controllers
 import { HealthController } from './health/health.controller'
 
 @Module( {
-  imports: [ CommonHttpModule ],
+  imports: [
+    ConfigModule.forRoot( { isGlobal: true } ), // Load environment variables from .env file
+    CommonHttpModule, 
+    RunsModule 
+  ],
   controllers: [ HealthController ],
 } )
 export class AppModule {}
