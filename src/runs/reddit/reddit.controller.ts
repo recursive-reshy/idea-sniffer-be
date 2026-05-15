3593bcc75f6aa9ab2a268e20b094b5e0b2b0b3d4
recursive-reshy <61969021+recursive-reshy@users.noreply.github.com>
2026-05-15 22:16:19 +0800

    feat: Added elapsed time

--- src/runs/reddit/reddit.controller.ts
+++ src/runs/reddit/reddit.controller.ts
@@ -3,14 +3,14 @@ import { Controller, Post, Body } from '@nestjs/common'
 // Services
 import { RedditService } from './reddit.service'
 // Types
-import type { FetchSubredditsPayload, RedditRecord } from '@app-types/Reddit'
+import type { FetchSubredditsPayload, RedditRunResult } from '@app-types/Reddit'
 
 @Controller( 'reddit' )
 export class RedditController {
   constructor( private readonly redditService: RedditService ) {}
 
   @Post( 'run' )
-  async startRun( @Body() body: FetchSubredditsPayload ): Promise< RedditRecord[] > {
+  async startRun( @Body() body: FetchSubredditsPayload ): Promise< RedditRunResult > {
     return await this.redditService.startRun( body )
   }
 }
