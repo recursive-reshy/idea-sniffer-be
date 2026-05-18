fe8801c26ded031fca03419ecd98b19396af95ab
recursive-reshy <61969021+recursive-reshy@users.noreply.github.com>
2026-05-18 17:42:17 +0800

    chore: formatting

--- src/cache/cache.module.ts
+++ src/cache/cache.module.ts
@@ -1,5 +1,5 @@
-import { Module } from "@nestjs/common";
-import { CacheService } from "./cache.service";
+import { Module } from '@nestjs/common'
+import { CacheService } from './cache.service'
 
 @Module( { 
   providers: [ CacheService ],
--- src/types/Signals.ts
+++ src/types/Signals.ts
@@ -1,10 +1,10 @@
 // TODO: Need to review once we have a better understanding of the data structure and requirements
 // Transformed output from scraped data
 export interface Signal {
-  sourceId: string;
-  rawText: string;
-  originUrl: string;
-  metadata: Record< string, unknown >;
-  fetchedAt: string;
-  provider: string;
+  sourceId: string
+  rawText: string
+  originUrl: string
+  metadata: Record< string, unknown >
+  fetchedAt: string
+  provider: string
 }
